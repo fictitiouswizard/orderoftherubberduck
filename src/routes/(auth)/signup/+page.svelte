611ea7c4a13e258b8282a0628d7443612bb275c4
Jason Sanders <fictitiouswizard@gmail.com>
2023-08-22 11:39:16 -0500

    updated register screen

--- src/routes/(auth)/signup/+page.svelte
+++ src/routes/(auth)/signup/+page.svelte
@@ -19,16 +19,25 @@
     </h1>
     <form method="post" use:enhance class="flex flex-col mt-2">
       <label for="username">Username</label>
-      <input id="username" name="username" />
+      <input
+        id="username"
+        name="username"
+        class="outline outline-1 rounded-md px-2 py-3 my-2 outline-gray-400"
+      />
       <label for="password">Password</label>
-      <input type="password" id="password" name="password" />
+      <input
+        type="password"
+        id="password"
+        name="password"
+        class="outline outline-1 rounded-md px-2 py-3 my-2 outline-gray-400"
+      />
       <button class="rounded-xl px-3 py-4 my-4 bg-blue-400 text-white font-bold shadow-md shadow-gray-400">
         Register
       </button>
     </form>
   </div>
   {#if form?.issues}
-    <div class="bg-white my-3">
+    <div class="bg-white text-red-500 my-3 rounded-3xl flex flex-col px-5 py-4">
       <ZodIssues issues={form.issues} />
     </div>
   {/if}
